6e6eb843c62cd1cbc042b2d11b9dc30005e317d7
Brian3647 <victoris3647@gmail.com>
2024-02-15 17:50:25 +0100

    :ambulance: I love fixing build scripts :tm:

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 mkdir -p dist
+
 bun build src/index.ts --outfile=dist/index.js --minify --target=browser
-cp src/*.html dist/
-cp src/*.css dist/
-cp manifest.json dist/
+
+cpdist() {
+	for file in $@; do
+		cp $file dist/ -r
+	done
+}
+
+cpdist src/*.html src/*.css manifest.json assets/*
